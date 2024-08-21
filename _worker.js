4f3aee0443f1afa5c2d087a0a584dce716db1ef1
loser <34392022+loserc6@users.noreply.github.com>
2024-08-21 13:43:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,7 +15,7 @@ let MainData = `
 `
 
 let urls = [];
-let subconverter = "subapi-loadbalancing.pages.dev"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/loserc6/CF-Workers-SUB/main/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
 
